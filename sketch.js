20941ae432c4a840853035b9a08b47493f89d592
NINJ444 <98404317+NINJ444@users.noreply.github.com>
2022-02-16 14:22:43 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
 //declaração das variáveis
 var trex, trex_running, edges;
 var solo, soloImg,soloinvisivel;
-
+var nuvem,nuvemimg;
+var Cacto1,Cacto2,Cacto3,Cacto4,Cacto5,Cacto6;
+var Pontos=0;
+var nuvens;
+var cactos;
+var START=1;
+var PLAY=2;
+var END=0;
+var estadodojogo=PLAY;
 //função de pré-carregamento
 function preload()
 {
+ nuvemimg=loadImage("cloud.png");
   trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
   soloImg = loadImage("ground2.png");
+  Cacto1 = loadImage("obstacle1.png");
+  Cacto2 = loadImage("obstacle2.png");
+  Cacto3 = loadImage("obstacle3.png");
+  Cacto4 = loadImage("obstacle4.png");
+  Cacto5 = loadImage("obstacle5.png");
+  Cacto6 = loadImage("obstacle6.png");
 }
 
 //função de configuração
@@ -28,17 +43,17 @@ function setup()
   //criar solo invisivel
   soloinvisivel=createSprite(50,190,400,10);
   soloinvisivel.visible=false;
+   cactos=new Group ();
+   nuvens=new Group ();
 }
 
 //desenho e animação
 function draw()
 {
-  background("lightgray");
-
-  //gerar número aleatório
-  var mega = Math.round(random(1,60));
-  console.log(mega);
-
+  background("white");
+  text("pontos: "+Pontos,500,20);
+  if(estadodojogo===PLAY){
+  
   //velocidade do solo
   solo.velocityX = -2;
   //console.log(solo.x);
@@ -55,18 +70,63 @@ function draw()
 
   //dar gravidade para o trex
   trex.velocityY = trex.velocityY + 0.5;
-
-  //trex colide com o solo
+  gerarCactos();
+  gerarNuvens();
+}
+  else if(estadodojogo===END){
+    cactos.setVelocityXEach(0);
+  }
+//trex colide com o solo
   trex.collide(soloinvisivel);
 
   //posição do trex no eixo y
   //console.log(trex.y);
+
+  //chamar a função da nuvem
+
+//exibir o frameCount
+//console.log(frameCount);
 
   drawSprites();
 
 }
 
 function gerarNuvens(){
-  
+  if(frameCount%60===0){
+    nuvem=createSprite(520,20,40,40);
+    nuvens.add(nuvem);
+    nuvem.y=Math.round(random(20,50));
+    nuvem.velocityX=-2;  
+    nuvem.addImage(nuvemimg);
+    //console.log(trex.depth);
+    //console.log(nuvem.depth);
+  trex.depth=nuvem.depth+1;
+  nuvem.lifetime=300;
+  }
 }
-
+function gerarCactos(){
+if(frameCount%60===0){
+var Cacto=createSprite(520,170,20,20);
+cactos.add(Cacto);
+Cacto.velocityX=-2;
+ //gerar número aleatório
+ var N = Math.round(random(1,6));
+switch(N){
+case 1:Cacto.addImage(Cacto1);
+break;
+case 2:Cacto.addImage(Cacto2);
+break;
+case 3:Cacto.addImage(Cacto3);
+break;
+case 4:Cacto.addImage(Cacto4);
+break;
+case 5:Cacto.addImage(Cacto5);
+break;
+case 6:Cacto.addImage(Cacto6);
+break;
+default:break;
+}
+Cacto.scale=0.5;
+Cacto.lifetime=300;
+}
+}
